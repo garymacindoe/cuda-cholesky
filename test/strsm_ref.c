ae9be5af0e8cc16764ba7729faaea103db82b643
Gary Macindoe <gary@garymacindoe.co.uk>
2012-09-02 21:18:50 +0000

    Added instruction counting to reference STRSM.

--- test/strsm_ref.c
+++ test/strsm_ref.c
@@ -13,16 +13,18 @@ static void strsm_ref(CBlasSide side, CBlasUplo uplo, CBlasTranspose trans,
     return;
   }
 
+  size_t * E = calloc(n * ldb, sizeof(size_t));
+
   if (side == CBlasLeft) {
     if (trans == CBlasNoTrans) {
       if (uplo == CBlasUpper) {
         for (size_t j = 0; j < n; j++) {
           size_t i = m - 1;
           do {
-            float temp = alpha * B[j * ldb + i];
-            for (size_t k = i + 1; k < m; k++)
-              temp -= A[k * lda + i] * B[j * ldb + k];
-            if (diag == CBlasNonUnit) temp /= A[i * lda + i];
+            float temp = alpha * B[j * ldb + i]; E[j * ldb + i] = 1;
+            for (size_t k = i + 1; k < m; k++) {
+              temp -= A[k * lda + i] * B[j * ldb + k]; E[j * ldb + i] += E[j * ldb + k] + 2; }
+            if (diag == CBlasNonUnit) { temp /= A[i * lda + i]; E[j * ldb + i]++; }
             B[j * ldb + i] = temp;
           } while (i-- > 0);
         }
@@ -30,10 +32,10 @@ static void strsm_ref(CBlasSide side, CBlasUplo uplo, CBlasTranspose trans,
       else {
         for (size_t j = 0; j < n; j++) {
           for (size_t i = 0; i < m; i++) {
-            float temp = alpha * B[j * ldb + i];
-            for (size_t k = 0; k < i; k++)
-              temp -= A[k * lda + i] * B[j * ldb + k];
-            if (diag == CBlasNonUnit) temp /= A[i * lda + i];
+            float temp = alpha * B[j * ldb + i]; E[j * ldb + i] = 1;
+            for (size_t k = 0; k < i; k++) {
+              temp -= A[k * lda + i] * B[j * ldb + k]; E[j * ldb + i] += E[j * ldb + k] + 2; }
+            if (diag == CBlasNonUnit) { temp /= A[i * lda + i]; E[j * ldb + i]++; }
             B[j * ldb + i] = temp;
           }
         }
@@ -43,10 +45,10 @@ static void strsm_ref(CBlasSide side, CBlasUplo uplo, CBlasTranspose trans,
       if (uplo == CBlasUpper) {
         for (size_t j = 0; j < n; j++) {
           for (size_t i = 0; i < m; i++) {
-            float temp = alpha * B[j * ldb + i];
-            for (size_t k = 0; k < i; k++)
-              temp -= A[i * lda + k] * B[j * ldb + k];
-            if (diag == CBlasNonUnit) temp /= A[i * lda + i];
+            float temp = alpha * B[j * ldb + i]; E[j * ldb + i] = 1;
+            for (size_t k = 0; k < i; k++) {
+              temp -= A[i * lda + k] * B[j * ldb + k]; E[j * ldb + i] += E[j * ldb + k] + 2; }
+            if (diag == CBlasNonUnit) { temp /= A[i * lda + i]; E[j * ldb + i]++; }
             B[j * ldb + i] = temp;
           }
         }
@@ -55,10 +57,10 @@ static void strsm_ref(CBlasSide side, CBlasUplo uplo, CBlasTranspose trans,
         for (size_t j = 0; j < n; j++) {
           size_t i = m - 1;
           do {
-            float temp = alpha * B[j * ldb + i];
-            for (size_t k = i + 1; k < m; k++)
-              temp -= A[i * lda + k] * B[j * ldb + k];
-            if (diag == CBlasNonUnit) temp /= A[i * lda + i];
+            float temp = alpha * B[j * ldb + i]; E[j * ldb + i] = 1;
+            for (size_t k = i + 1; k < m; k++) {
+              temp -= A[i * lda + k] * B[j * ldb + k]; E[j * ldb + i] += E[j * ldb + k] + 2; }
+            if (diag == CBlasNonUnit) { temp /= A[i * lda + i]; E[j * ldb + i]++; }
             B[j * ldb + i] = temp;
           } while (i-- > 0);
         }
@@ -70,10 +72,10 @@ static void strsm_ref(CBlasSide side, CBlasUplo uplo, CBlasTranspose trans,
       if (uplo == CBlasUpper) {
         for (size_t j = 0; j < n; j++) {
           for (size_t i = 0; i < m; i++) {
-            float temp = alpha * B[j * ldb + i];
-            for (size_t k = j + 1; k < n; k++)
-              temp -= A[j * lda + k] * B[k * ldb + i];
-            if (diag == CBlasNonUnit) temp /= A[j * lda + j];
+            float temp = alpha * B[j * ldb + i]; E[j * ldb + i] = 1;
+            for (size_t k = j + 1; k < n; k++) {
+              temp -= A[j * lda + k] * B[k * ldb + i]; E[j * ldb + i] += E[k * ldb + i] + 2; }
+            if (diag == CBlasNonUnit) { temp /= A[j * lda + j]; E[j * ldb + i]++; }
             B[j * ldb + i] = temp;
           }
         }
@@ -81,10 +83,10 @@ static void strsm_ref(CBlasSide side, CBlasUplo uplo, CBlasTranspose trans,
       else {
         for (size_t j = 0; j < n; j++) {
           for (size_t i = 0; i < m; i++) {
-            float temp = alpha * B[j * ldb + i];
-            for (size_t k = 0; k < j; k++)
-              temp -= A[j * lda + k] * B[k * ldb + i];
-            if (diag == CBlasNonUnit) temp /= A[j * lda + j];
+            float temp = alpha * B[j * ldb + i]; E[j * ldb + i] = 1;
+            for (size_t k = 0; k < j; k++) {
+              temp -= A[j * lda + k] * B[k * ldb + i]; E[j * ldb + i] += E[k * ldb + i] + 2; }
+            if (diag == CBlasNonUnit) { temp /= A[j * lda + j]; E[j * ldb + i]++; }
             B[j * ldb + i] = temp;
           }
         }
@@ -94,10 +96,10 @@ static void strsm_ref(CBlasSide side, CBlasUplo uplo, CBlasTranspose trans,
       if (uplo == CBlasUpper) {
         for (size_t j = 0; j < n; j++) {
           for (size_t i = 0; i < m; i++) {
-            float temp = alpha * B[j * ldb + i];
-            for (size_t k = 0; k < j; k++)
-              temp -= A[k * lda + j] * B[k * ldb + i];
-            if (diag == CBlasNonUnit) temp /= A[j * lda + j];
+            float temp = alpha * B[j * ldb + i]; E[j * ldb + i] = 1;
+            for (size_t k = 0; k < j; k++) {
+              temp -= A[k * lda + j] * B[k * ldb + i]; E[j * ldb + i] += E[k * ldb + i] + 2; }
+            if (diag == CBlasNonUnit) { temp /= A[j * lda + j]; E[j * ldb + i]++; }
             B[j * ldb + i] = temp;
           }
         }
@@ -105,14 +107,22 @@ static void strsm_ref(CBlasSide side, CBlasUplo uplo, CBlasTranspose trans,
       else {
         for (size_t j = 0; j < n; j++) {
           for (size_t i = 0; i < m; i++) {
-            float temp = alpha * B[j * ldb + i];
-            for (size_t k = j + 1; k < n; k++)
-              temp -= A[k * lda + j] * B[k * ldb + i];
-            if (diag == CBlasNonUnit) temp /= A[j * lda + j];
+            float temp = alpha * B[j * ldb + i]; E[j * ldb + i] = 1;
+            for (size_t k = j + 1; k < n; k++) {
+              temp -= A[k * lda + j] * B[k * ldb + i]; E[j * ldb + i] += E[k * ldb + i] + 2; }
+            if (diag == CBlasNonUnit) { temp /= A[j * lda + j]; E[j * ldb + i]++; }
             B[j * ldb + i] = temp;
           }
         }
       }
     }
   }
+
+  for (size_t i = 0; i < m; i++) {
+    for (size_t j = 0; j < n; j++)
+      fprintf(stderr, "%4zu", E[j * ldb + i]);
+    fprintf(stderr, "\n");
+  }
+
+  free(E);
 }
