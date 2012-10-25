5c466fd58f3782c81dbf11e443df4d76d5c4a83e
Gary Macindoe <gary@garymacindoe.co.uk>
2012-10-25 22:09:25 +0000

    Cleaned up code in GPU SSYRK in attempt to speed it up.

--- src/blas/ssyrk.cu
+++ src/blas/ssyrk.cu
@@ -10,8 +10,8 @@ __device__ void saxpy(float alpha, const float * x, float * y) {
 
 /**
  * SSYRK:
- *   C := alpha * A'A + beta * C for trans == CBlasNoTrans
- *   C := alpha * AA' + beta * C for trans != CBlasNoTrans
+ *   C := alpha * AA' + beta * C for trans == CBlasNoTrans
+ *   C := alpha * A'A + beta * C for trans != CBlasNoTrans
  *
  * Only the upper or lower triangle of C is updated.
  *
@@ -26,8 +26,8 @@ __device__ void saxpy(float alpha, const float * x, float * y) {
 template <CBlasUplo uplo, CBlasTranspose trans,
           unsigned int mb, unsigned int nb, unsigned int kb,
           unsigned int bx, unsigned int by>
-__global__ void ssyrk(int n, int k, float alpha,
-                      const float * __restrict__ A, int lda,
+__global__ void ssyrk(int n, int k,
+                      float alpha, const float * __restrict__ A, int lda,
                       float beta, float * __restrict__ C, int ldc) {
 
 //   int bi, bj, nnb = (n + nb - 1) / nb;
@@ -51,41 +51,48 @@ __global__ void ssyrk(int n, int k, float alpha,
 //   bi *= mb;
 //   bj *= nb;
 
-  const int bi = blockIdx.x * mb;
-  const int bj = blockIdx.y * nb;
-
-  const int ti = threadIdx.y * bx + threadIdx.x;        // Unwrapped thread index [0, bx * by]
-
-  /*
-   * Cause blocks that are entirely above or below the diagonal to exit now.
-   */
-//   if (mb == nb && blockIdx.y < blockIdx.x) return;
-  if ((uplo == CBlasUpper && bj + nb - 1 < bi) ||
-      (uplo == CBlasLower && bi + mb - 1 < bj))
-    return;
-
-  /*
-   * If trans == CBlasNoTrans, A is A and B is A'. If trans != CBlasNoTrans, A
-   * is A' and B is A.
-   */
+  // Using an SGEMM kernel, SSYRK is:
+  // C = alpha * A * B + beta * C
+  // with A = A and B = A' when trans == CBlasNoTrans, and
+  // with A = A' and B = A when trans == CBlasTrans
   const float * __restrict__ B = A;
 
+  const int bi = blockIdx.x * mb;       // Starting row of block of C
+  const int bj = blockIdx.y * nb;       // Starting column of block of C
+  const int ti = threadIdx.y * bx + threadIdx.x;        // Unwrapped thread index [0, bx * by]
+
+  if (uplo == CBlasUpper) {
+    if (bj + nb - 1 < bi)
+      return;
+  }
+  else if (uplo == CBlasLower) {
+    if (bi + mb - 1 < bj)
+      return;
+  }
+
   /*
-   * Compute our starting points in A, "B" and C.
+   * Compute our starting points in A, B and C.
    *
-   * For trans != CBlasNoTrans A is cached in shared memory so the unwrapped
+   * For trans == CBlasTrans A is cached in shared memory so the unwrapped
    * thread index can be re-wrapped around mb when calculating C.
    *
    * If trans == CBlasNoTrans then bx * by == mb (checked later on) so there
    * doesn't need to be a separate check for trans == CBlasNoTrans in
    * calculating the start of C here.
    */
-  A += (trans == CBlasNoTrans) ? bi + ti : (bi + threadIdx.y) * lda + threadIdx.x;
-  B += (trans == CBlasNoTrans) ? threadIdx.y * lda + bj + threadIdx.x : (bj + threadIdx.y) * lda + threadIdx.x;
-  C += (bx * by == mb) ? bj * ldc + bi + ti : (bj + 16 * (ti / mb)) * ldc + bi + ti % mb;
+  if (trans == CBlasNoTrans) {
+    A += bi + ti;
+    B += threadIdx.y * lda + bj + threadIdx.x;
+    C += bj * ldc + bi + ti;
+  }
+  else {
+    A += (bi + threadIdx.y) * lda + threadIdx.x;
+    B += (bj + threadIdx.y) * lda + threadIdx.x;
+    C += (bj + 16 * (ti / mb)) * ldc + bi + ti % mb;
+  }
 
   /*
-   * Blocks of A and "B" in shared memory and C in registers.
+   * Blocks of A and B in shared memory and C in registers.
    */
   __shared__ float a[mb][kb + 1];       // Optimised away when trans == CBlasNoTrans
   __shared__ float b[kb][(trans == CBlasNoTrans) ? nb : nb + 1];
@@ -95,8 +102,8 @@ __global__ void ssyrk(int n, int k, float alpha,
 
   while (k > 0) {
     if (trans == CBlasNoTrans) {
-//       typedef char x[(nb % bx == 0) ? 1 : -1];  // bx must be a multiple of nb
-//       typedef char y[(kb % by == 0) ? 1 : -1];  // by must be a multiple of kb
+      // C = aAA' + bC so read B into shared memory and transpose leaving A
+      // untransposed in global memory
 #pragma unroll
       for (int l = 0; l < kb; l += by) {
 #pragma unroll
@@ -105,9 +112,8 @@ __global__ void ssyrk(int n, int k, float alpha,
       }
     }
     else {
-//       typedef char x[(kb % bx == 0) ? 1 : -1];  // bx must be a multiple of kb
-//       typedef char y[(mb % by == 0) ? 1 : -1];  // by must be a multiple of mb
-//       typedef char z[(nb % by == 0) ? 1 : -1];  // by must be a multiple of nb
+      // C = aA'A + bC so read A into shared memory and transpose before reading
+      // B into shared memory untransposed
 #pragma unroll
       for (int l = 0; l < kb; l += bx) {
 #pragma unroll
@@ -129,8 +135,7 @@ __global__ void ssyrk(int n, int k, float alpha,
     if (k < kb) break;
 
     if (trans == CBlasNoTrans) {
-//       typedef char x[(bx * by == mb) ? 1 : -1]; // There must be mb unrolled threads
-//       typedef char y[(nb == 16) ? 1 : -1]; // nb must equal the size of row per thread
+      // Read A from global memory
 #pragma unroll
       for (int l = 0; l < kb; l++) {
         saxpy(A[0], b[l], c);
@@ -138,12 +143,10 @@ __global__ void ssyrk(int n, int k, float alpha,
       }
     }
     else {
-//       typedef char x[(bx * by % mb == 0) ? 1 : -1];     // bx * by must be a multiple of mb
-//       typedef char y[((bx * by * 16) / mb == nb) ? 1 : -1];     // when the threads are wrapped around mb they must spread along to nb
+      // Read A from shared memory
 #pragma unroll
       for (int l = 0; l < kb; l++)
-        saxpy(a[(bx * by == mb) ? ti : ti % mb][l],
-              &b[l][(bx * by == mb) ? 0 : 16 * (ti / mb)], c);
+        saxpy(a[ti % mb][l], &b[l][16 * (ti / mb)], c);
     }
 
     __syncthreads();
@@ -153,57 +156,59 @@ __global__ void ssyrk(int n, int k, float alpha,
   }
 
   if (trans == CBlasNoTrans) {
+    // Read A from global memory
     for (int l = 0; l < k; l++) {
       saxpy(A[0], b[l], c);
       A += lda;
     }
   }
   else {
+    // Read A' from shared memory
     for (int l = 0; l < k; l++)
-      saxpy(a[(bx * by == mb) ? ti : ti % mb][l],
-            &b[l][(bx * by == mb) ? 0 : 16 * (ti / mb)], c);
+      saxpy(a[ti % mb][l], &b[l][16 * (ti / mb)], c);
   }
 
-  const unsigned int i = (bx * by == mb) ? bi + ti : bi + ti % mb;
-  const unsigned int j = (bx * by == mb) ? bj : bj + 16 * (ti / mb);
-  if (i < n) {
-    n -= j;
-    if (n <= 0) return;
+  if (bx * by == mb)
+    n -= bj;
+  else
+    n -= bj + 16 * (ti / mb);
+  if (n <= 0) return;
+  if ((bx * by == mb && bi + ti < n) || (bx * by > mb && bi + ti % mb < n)) {
     if (beta == 0.0f) {
-      if ((uplo == CBlasUpper && i <= j +  0) || (uplo == CBlasLower && i >= j +  0)) C[0] = alpha * c[ 0]; if ( 1 >= n) return; C += ldc;
-      if ((uplo == CBlasUpper && i <= j +  1) || (uplo == CBlasLower && i >= j +  1)) C[0] = alpha * c[ 1]; if ( 2 >= n) return; C += ldc;
-      if ((uplo == CBlasUpper && i <= j +  2) || (uplo == CBlasLower && i >= j +  2)) C[0] = alpha * c[ 2]; if ( 3 >= n) return; C += ldc;
-      if ((uplo == CBlasUpper && i <= j +  3) || (uplo == CBlasLower && i >= j +  3)) C[0] = alpha * c[ 3]; if ( 4 >= n) return; C += ldc;
-      if ((uplo == CBlasUpper && i <= j +  4) || (uplo == CBlasLower && i >= j +  4)) C[0] = alpha * c[ 4]; if ( 5 >= n) return; C += ldc;
-      if ((uplo == CBlasUpper && i <= j +  5) || (uplo == CBlasLower && i >= j +  5)) C[0] = alpha * c[ 5]; if ( 6 >= n) return; C += ldc;
-      if ((uplo == CBlasUpper && i <= j +  6) || (uplo == CBlasLower && i >= j +  6)) C[0] = alpha * c[ 6]; if ( 7 >= n) return; C += ldc;
-      if ((uplo == CBlasUpper && i <= j +  7) || (uplo == CBlasLower && i >= j +  7)) C[0] = alpha * c[ 7]; if ( 8 >= n) return; C += ldc;
-      if ((uplo == CBlasUpper && i <= j +  8) || (uplo == CBlasLower && i >= j +  8)) C[0] = alpha * c[ 8]; if ( 9 >= n) return; C += ldc;
-      if ((uplo == CBlasUpper && i <= j +  9) || (uplo == CBlasLower && i >= j +  9)) C[0] = alpha * c[ 9]; if (10 >= n) return; C += ldc;
-      if ((uplo == CBlasUpper && i <= j + 10) || (uplo == CBlasLower && i >= j + 10)) C[0] = alpha * c[10]; if (11 >= n) return; C += ldc;
-      if ((uplo == CBlasUpper && i <= j + 11) || (uplo == CBlasLower && i >= j + 11)) C[0] = alpha * c[11]; if (12 >= n) return; C += ldc;
-      if ((uplo == CBlasUpper && i <= j + 12) || (uplo == CBlasLower && i >= j + 12)) C[0] = alpha * c[12]; if (13 >= n) return; C += ldc;
-      if ((uplo == CBlasUpper && i <= j + 13) || (uplo == CBlasLower && i >= j + 13)) C[0] = alpha * c[13]; if (14 >= n) return; C += ldc;
-      if ((uplo == CBlasUpper && i <= j + 14) || (uplo == CBlasLower && i >= j + 14)) C[0] = alpha * c[14]; if (15 >= n) return; C += ldc;
-      if ((uplo == CBlasUpper && i <= j + 15) || (uplo == CBlasLower && i >= j + 15)) C[0] = alpha * c[15];
+      C[0] = alpha * c[ 0]; if ( 1 >= n) return; C += ldc;
+      C[0] = alpha * c[ 1]; if ( 2 >= n) return; C += ldc;
+      C[0] = alpha * c[ 2]; if ( 3 >= n) return; C += ldc;
+      C[0] = alpha * c[ 3]; if ( 4 >= n) return; C += ldc;
+      C[0] = alpha * c[ 4]; if ( 5 >= n) return; C += ldc;
+      C[0] = alpha * c[ 5]; if ( 6 >= n) return; C += ldc;
+      C[0] = alpha * c[ 6]; if ( 7 >= n) return; C += ldc;
+      C[0] = alpha * c[ 7]; if ( 8 >= n) return; C += ldc;
+      C[0] = alpha * c[ 8]; if ( 9 >= n) return; C += ldc;
+      C[0] = alpha * c[ 9]; if (10 >= n) return; C += ldc;
+      C[0] = alpha * c[10]; if (11 >= n) return; C += ldc;
+      C[0] = alpha * c[11]; if (12 >= n) return; C += ldc;
+      C[0] = alpha * c[12]; if (13 >= n) return; C += ldc;
+      C[0] = alpha * c[13]; if (14 >= n) return; C += ldc;
+      C[0] = alpha * c[14]; if (15 >= n) return; C += ldc;
+      C[0] = alpha * c[15];
     }
     else {
-      if ((uplo == CBlasUpper && i <= j +  0) || (uplo == CBlasLower && i >= j +  0)) C[0] = alpha * c[ 0] + beta * C[0]; if ( 1 >= n) return; C += ldc;
-      if ((uplo == CBlasUpper && i <= j +  1) || (uplo == CBlasLower && i >= j +  1)) C[0] = alpha * c[ 1] + beta * C[0]; if ( 2 >= n) return; C += ldc;
-      if ((uplo == CBlasUpper && i <= j +  2) || (uplo == CBlasLower && i >= j +  2)) C[0] = alpha * c[ 2] + beta * C[0]; if ( 3 >= n) return; C += ldc;
-      if ((uplo == CBlasUpper && i <= j +  3) || (uplo == CBlasLower && i >= j +  3)) C[0] = alpha * c[ 3] + beta * C[0]; if ( 4 >= n) return; C += ldc;
-      if ((uplo == CBlasUpper && i <= j +  4) || (uplo == CBlasLower && i >= j +  4)) C[0] = alpha * c[ 4] + beta * C[0]; if ( 5 >= n) return; C += ldc;
-      if ((uplo == CBlasUpper && i <= j +  5) || (uplo == CBlasLower && i >= j +  5)) C[0] = alpha * c[ 5] + beta * C[0]; if ( 6 >= n) return; C += ldc;
-      if ((uplo == CBlasUpper && i <= j +  6) || (uplo == CBlasLower && i >= j +  6)) C[0] = alpha * c[ 6] + beta * C[0]; if ( 7 >= n) return; C += ldc;
-      if ((uplo == CBlasUpper && i <= j +  7) || (uplo == CBlasLower && i >= j +  7)) C[0] = alpha * c[ 7] + beta * C[0]; if ( 8 >= n) return; C += ldc;
-      if ((uplo == CBlasUpper && i <= j +  8) || (uplo == CBlasLower && i >= j +  8)) C[0] = alpha * c[ 8] + beta * C[0]; if ( 9 >= n) return; C += ldc;
-      if ((uplo == CBlasUpper && i <= j +  9) || (uplo == CBlasLower && i >= j +  9)) C[0] = alpha * c[ 9] + beta * C[0]; if (10 >= n) return; C += ldc;
-      if ((uplo == CBlasUpper && i <= j + 10) || (uplo == CBlasLower && i >= j + 10)) C[0] = alpha * c[10] + beta * C[0]; if (11 >= n) return; C += ldc;
-      if ((uplo == CBlasUpper && i <= j + 11) || (uplo == CBlasLower && i >= j + 11)) C[0] = alpha * c[11] + beta * C[0]; if (12 >= n) return; C += ldc;
-      if ((uplo == CBlasUpper && i <= j + 12) || (uplo == CBlasLower && i >= j + 12)) C[0] = alpha * c[12] + beta * C[0]; if (13 >= n) return; C += ldc;
-      if ((uplo == CBlasUpper && i <= j + 13) || (uplo == CBlasLower && i >= j + 13)) C[0] = alpha * c[13] + beta * C[0]; if (14 >= n) return; C += ldc;
-      if ((uplo == CBlasUpper && i <= j + 14) || (uplo == CBlasLower && i >= j + 14)) C[0] = alpha * c[14] + beta * C[0]; if (15 >= n) return; C += ldc;
-      if ((uplo == CBlasUpper && i <= j + 15) || (uplo == CBlasLower && i >= j + 15)) C[0] = alpha * c[15] + beta * C[0];
+      C[0] = alpha * c[ 0] + beta * C[0]; if ( 1 >= n) return; C += ldc;
+      C[0] = alpha * c[ 1] + beta * C[0]; if ( 2 >= n) return; C += ldc;
+      C[0] = alpha * c[ 2] + beta * C[0]; if ( 3 >= n) return; C += ldc;
+      C[0] = alpha * c[ 3] + beta * C[0]; if ( 4 >= n) return; C += ldc;
+      C[0] = alpha * c[ 4] + beta * C[0]; if ( 5 >= n) return; C += ldc;
+      C[0] = alpha * c[ 5] + beta * C[0]; if ( 6 >= n) return; C += ldc;
+      C[0] = alpha * c[ 6] + beta * C[0]; if ( 7 >= n) return; C += ldc;
+      C[0] = alpha * c[ 7] + beta * C[0]; if ( 8 >= n) return; C += ldc;
+      C[0] = alpha * c[ 8] + beta * C[0]; if ( 9 >= n) return; C += ldc;
+      C[0] = alpha * c[ 9] + beta * C[0]; if (10 >= n) return; C += ldc;
+      C[0] = alpha * c[10] + beta * C[0]; if (11 >= n) return; C += ldc;
+      C[0] = alpha * c[11] + beta * C[0]; if (12 >= n) return; C += ldc;
+      C[0] = alpha * c[12] + beta * C[0]; if (13 >= n) return; C += ldc;
+      C[0] = alpha * c[13] + beta * C[0]; if (14 >= n) return; C += ldc;
+      C[0] = alpha * c[14] + beta * C[0]; if (15 >= n) return; C += ldc;
+      C[0] = alpha * c[15] + beta * C[0];
     }
   }
 }
@@ -249,7 +254,7 @@ __global__ void ssyrk(int n, int k, float alpha,
  * kb is chosen to be the largest multiple of 16 such that the number of blocks
  * per multiprocessor is limited by the register usage.
  */
-template void ssyrk<CBlasUpper, CBlasNoTrans, 64, 16, 16, 16,  4>(int, int, float, const float *, int, float, float *, int);
-template void ssyrk<CBlasLower, CBlasNoTrans, 64, 16, 16, 16,  4>(int, int, float, const float *, int, float, float *, int);
-template void ssyrk<CBlasUpper, CBlasTrans,   32, 32,  8,  8,  8>(int, int, float, const float *, int, float, float *, int);
-template void ssyrk<CBlasLower, CBlasTrans,   32, 32,  8,  8,  8>(int, int, float, const float *, int, float, float *, int);
+template void ssyrk<CBlasUpper, CBlasNoTrans, 64, 16, 16, 16,  4>(int, int, float, const float * __restrict__, int, float, float * __restrict__, int);
+template void ssyrk<CBlasLower, CBlasNoTrans, 64, 16, 16, 16,  4>(int, int, float, const float * __restrict__, int, float, float * __restrict__, int);
+template void ssyrk<CBlasUpper, CBlasTrans,   32, 32,  8,  8,  8>(int, int, float, const float * __restrict__, int, float, float * __restrict__, int);
+template void ssyrk<CBlasLower, CBlasTrans,   32, 32,  8,  8,  8>(int, int, float, const float * __restrict__, int, float, float * __restrict__, int);
