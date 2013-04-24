a376b58c406b5bdd68d29aa9b43aaa2a0f31ba30
Gary Macindoe <gary@garymacindoe.co.uk>
2013-04-24 18:49:55 +0000

    Ported trmm LAPACK to double.

---
+++ test/lapack/ref/dpotri_ref.c
@@ -0,0 +1,67 @@
+static void dpotri_ref(CBlasUplo uplo, size_t n,
+                       double * restrict A, size_t lda, long * restrict info) {
+  *info = 0;
+  if (lda < n)
+    *info = -5;
+  if (*info != 0) {
+    XERBLA(-(*info));
+    return;
+  }
+
+  if (n == 0)
+    return;
+
+  if (uplo == CBlasUpper) {
+    for (size_t j = 0; j < n; j++) {
+      if (A[j * lda + j] == 0.0) {
+        *info = (long)j + 1;
+        return;
+      }
+      A[j * lda + j] = 1.0 / A[j * lda + j];
+      double ajj = -A[j * lda + j];
+
+      for (size_t i = 0; i < j; i++) {
+        double temp = A[j * lda + i] * A[i * lda + i];
+        for (size_t k = i + 1; k < j; k++)
+          temp += A[k * lda + i] * A[j * lda + k];
+        A[j * lda + i] = temp * ajj;
+      }
+    }
+
+    for (size_t j = 0; j < n; j++) {
+      double ajj = A[j * lda + j];
+      for (size_t i = 0; i <= j; i++) {
+        A[j * lda + i] *= ajj;
+        for (size_t k = j + 1; k < n; k++)
+          A[j * lda + i] += A[k * lda + i] * A[k * lda + j];
+      }
+    }
+  }
+  else {
+    size_t j = n - 1;
+    do {
+      if (A[j * lda + j] == 0.0) {
+        *info = (long)j + 1;
+        return;
+      }
+      A[j * lda + j] = 1.0 / A[j * lda + j];
+      double ajj = -A[j * lda + j];
+
+      for (size_t i = n - 1; i > j; i--) {
+        double temp = A[j * lda + i] * A[i * lda + i];
+        for (size_t k = j + 1; k < i; k++)
+          temp += A[k * lda + i] * A[j * lda + k];
+        A[j * lda + i] = temp * ajj;
+      }
+    } while (j-- > 0);
+
+    for (size_t i = 0; i < n; i++) {
+      double aii = A[i * lda + i];
+      for (size_t j = 0; j <= i; j++) {
+        A[j * lda + i] *= aii;
+        for (size_t k = i + 1; k < n; k++)
+          A[j * lda + i] += A[i * lda + k] * A[j * lda + k];
+      }
+    }
+  }
+}
